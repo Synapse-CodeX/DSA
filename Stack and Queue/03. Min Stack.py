36a18ca37d350983bb1209226d6a407ee5f252b0
Debshuvra Sarkar <debshuvra2005@gmail.com>
2026-08-08 12:11:05 +0530

    LT 155 done

--- Stack and Queue/03. Min Stack.py
+++ Stack and Queue/03. Min Stack.py
@@ -1,2 +1,22 @@
 # Leetcode 155
 
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value: int) -> None:
+        if not self.stack:
+            self.stack.append([value,value])
+        else :
+            curr_min = self.stack[-1][1]
+            self.stack.append([value,min(value,curr_min)])
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
