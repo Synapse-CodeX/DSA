2253657e4ba6780b2ea960fb21378c2e2d61224a
Debshuvra Sarkar <debshuvra2005@gmail.com>
2026-08-09 01:25:19 +0530

    Add implementation for asteroid collision algorithm

---
+++ Stack and Queue/06. Asteroid Collision.py
@@ -0,0 +1,28 @@
+# LEETCODE 735
+
+from typing import List
+
+
+class Solution:
+
+    def asteroidCollision(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        st = []
+
+        for i in range(0, n):
+            if nums[i] > 0:
+                st.append(nums[i])
+            else:
+                while (
+                    len(st) != 0
+                    and st[-1] > 0
+                    and st[-1] < abs(nums[i])
+                ):
+                    st.pop()
+
+                if len(st) != 0 and st[-1] == abs(nums[i]):
+                    st.pop()
+                elif len(st) == 0 or st[-1] < 0:
+                    st.append(nums[i])
+
+        return st
